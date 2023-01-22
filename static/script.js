843f6678d2073a16093fa6c30732de20eab673cb
darksh3ll <darksh3ll.js@gmail.com>
2023-01-22 09:40:36 +0100

    integration function change colors

--- static/script.js
+++ static/script.js
@@ -8,7 +8,19 @@ const topicIdMap = {
     'bureau/temperature': 'bureau_temperature'
 };
 
+
+function changeTempColor(data){
+
+}
+
+
+function changeHumidityColor(){
+
+}
+
 socket.on('mqtt_message', function (data) {
+    console.log(data)
+    const room = data.topic.split('/')[1]
     const elementId = topicIdMap[data.topic];
     if (elementId) {
         document.getElementById(elementId).innerHTML = data.payload;
@@ -17,36 +29,36 @@ socket.on('mqtt_message', function (data) {
     }
 });
 
-function updateUI(data) {
-    console.log(data)
-    // Mise à jour de l'élément HTML avec l'ID "temperature" avec les données reçues
-    // document.getElementById('temperature').textContent = data;
-}
-
-// Gestion de l'événement "mqtt_message" envoyé par le serveur
-socket.on('mqtt_message', function (data) {
-    // console.log(data)
-    // document.querySelector('#name').innerHTML = data.topic.split('/')[0]
-    // if (data.topic === 'bureau/humidity') {
-    //     humidity.innerHTML = data.payload;
-    //     if (+data.payload > 70) {
-    //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
-    //         humidity.classList.add("text-red-500");
-    //     } else if (+data.payload > 60 && +data.payload <=70) {
-    //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
-    //         humidity.classList.add("text-yellow-500");
-    //     } else {
-    //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
-    //         humidity.classList.add("text-green-700");
-    //     }
-    // }
-    // if (data.topic === 'bureau/temperature') {
-    //     document.querySelector("#temperature").innerHTML = data.payload;
-    // }
-    console.log('Données reçues du serveur:', data);
-    // Mise à jour de l'interface utilisateur avec les données reçues
-    // updateUI(data);
-});
-
-
-
+// function updateUI(data) {
+//     console.log(data)
+//     // Mise à jour de l'élément HTML avec l'ID "temperature" avec les données reçues
+//     // document.getElementById('temperature').textContent = data;
+// }
+//
+// // Gestion de l'événement "mqtt_message" envoyé par le serveur
+// socket.on('mqtt_message', function (data) {
+//     // console.log(data)
+//     // document.querySelector('#name').innerHTML = data.topic.split('/')[0]
+//     // if (data.topic === 'bureau/humidity') {
+//     //     humidity.innerHTML = data.payload;
+//     //     if (+data.payload > 70) {
+//     //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
+//     //         humidity.classList.add("text-red-500");
+//     //     } else if (+data.payload > 60 && +data.payload <=70) {
+//     //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
+//     //         humidity.classList.add("text-yellow-500");
+//     //     } else {
+//     //         humidity.classList.remove("text-red-500","text-yellow-500","text-green-700")
+//     //         humidity.classList.add("text-green-700");
+//     //     }
+//     // }
+//     // if (data.topic === 'bureau/temperature') {
+//     //     document.querySelector("#temperature").innerHTML = data.payload;
+//     // }
+//     console.log('Données reçues du serveur:', data);
+//     // Mise à jour de l'interface utilisateur avec les données reçues
+//     // updateUI(data);
+// });
+//
+//
+//
